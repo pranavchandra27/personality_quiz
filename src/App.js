2f58507769e85e48fb2768dc48331b26e6af3452
pranavchandra27 <pranavchandra27@gmail.com>
2020-08-05 15:47:19 +0530

    quiz works and can select answer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div className='App'>Quiz App</div>;
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
